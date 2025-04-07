f581ab3e1719206b01ff2ff6e6123bbd5d4c9167
Vlad Fil <vlad.fil.mobile@gmail.com>
2025-04-07 16:55:31 +0300

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,8 @@
 function makeTransaction(quantity, pricePerDroid) {
-  const value = `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
-  return value;
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
+
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
